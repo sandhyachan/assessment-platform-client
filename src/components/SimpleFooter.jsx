9d9d053c24eb00630f1af0eded930ae20d24e8f9
sandhyachan <sandhyachan6@gmail.com>
2025-01-22 19:54:27 +0530

    feat: create simple footer with copyright information and basic links

---
+++ src/components/SimpleFooter.jsx
@@ -0,0 +1,10 @@
+export default function SimpleFooter() {
+  return (
+    <>
+    {/* Footer */}
+    <footer className="footer text-center py-3 mt-auto">
+        <p className="mb-0">© 2025 Questra. All rights reserved.</p>
+      </footer>
+    </>
+  )
+}
